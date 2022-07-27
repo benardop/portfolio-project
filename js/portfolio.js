b9a928355681c3bf72c632e6916d5ce535ea9d52
benardop <benard.oc.p@gmail.com>
2022-07-27 17:42:55 +0300

    Remove ESlinter errors from javascript file

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,18 @@
-let bugger_menu = document.querySelector("#bugger-menu");
-let ul = document.querySelector("nav ul");
-let navigation = document.querySelector("nav");
-let btnClose = document.querySelector("#navbar");
+const buggerMenu = document.querySelector('#bugger-menu');
+const ul = document.querySelector('nav ul');
+// const navigation = document.querySelector('nav');
+const btnClose = document.querySelector('#navbar');
 
-let navigation_link = document.querySelectorAll(".nav-link");
+const navigationLink = document.querySelectorAll('.nav-link');
 
-
-bugger_menu.addEventListener("click", () => {
-    ul.classList.toggle("show");
+buggerMenu.addEventListener('click', () => {
+  ul.classList.toggle('show');
 });
 
-navigation_link.forEach((navLink) => 
-    navLink.addEventListener("click", () => {
-        ul.classList.remove("remove");
-    })
-);
+navigationLink.forEach((navLink) => navLink.addEventListener('click', () => {
+  ul.classList.remove('remove');
+}));
 
-btnClose.addEventListener("click", () => {
-    ul.classList.toggle("show");
-})
+btnClose.addEventListener('click', () => {
+  ul.classList.toggle('show');
+});
