073841652d2dd2eea3ab45d575762753ff74c9ad
benardop <benard.oc.p@gmail.com>
2022-07-29 23:31:13 +0300

    Add Js code modal popup

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,5 @@
 const buggerMenu = document.querySelector('#bugger-menu');
 const ul = document.querySelector('nav ul');
-// const navigation = document.querySelector('nav');
 const btnClose = document.querySelector('#navbar');
 
 const navigationLink = document.querySelectorAll('.nav-link');
@@ -18,86 +17,84 @@ btnClose.addEventListener('click', () => {
 });
 
 const projectsData = [
-    {
-      name: 'Tonic',
-      client: 'CANOPY',
-      id: 1,
-      year: 2015,
-      image: '/image/snapshoot-portfolio1.png',
-      technologies: ['html', 'css', 'javascript'],
-      description:
+  {
+    name: 'Tonic',
+    client: 'CANOPY',
+    id: 1,
+    year: 2015,
+    image: '/image/snapshoot-portfolio1.png',
+    technologies: ['html', 'css', 'javascript'],
+    description:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
         + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
         + 'when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem '
         + 'Ipsum is simply dummy text of the printing and typesetting industry.',
-      liveVersion: 'https://github.com/benardop/portfolio-project',
-      source: 'https://github.com/benardop/portfolio-project',
-    },
-    {
-      name: 'Multi post Stories',
-      client: 'CANOPY',
-      id: 2,
-      year: 2020,
-      image: '/image/snapshoot-portfolio2.png',
-      technologies: ['html', 'css', 'javascript'],
-      description:
+    liveVersion: 'https://github.com/benardop/portfolio-project',
+    source: 'https://github.com/benardop/portfolio-project',
+  },
+  {
+    name: 'Multi post Stories',
+    client: 'CANOPY',
+    id: 2,
+    year: 2020,
+    image: '/image/snapshoot-portfolio2.png',
+    technologies: ['html', 'css', 'javascript'],
+    description:
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
         + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
         + 'when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem '
         + 'Ipsum is simply dummy text of the printing and typesetting industry.',
-      liveVersion: 'https://github.com/benardop/portfolio-project',
-      source: 'https://github.com/benardop/portfolio-project',
-    },
-  
-    {
-      name: 'Tonic',
-      client: 'CANOPY',
-      year: 2019,
-      id: 3,
-      image: '/image/snapshoot-portfolio3.png',
-      technologies: ['html', 'css', 'javascript'],
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-        + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
-        + 'when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem '
-        + 'Ipsum is simply dummy text of the printing and typesetting industry.',
-      liveVersion: 'https://github.com/benardop/portfolio-project',
-      source: 'https://github.com/benardop/portfolio-project',
-    },
-  
-    {
-      name: 'Multi post Stories',
-      client: 'CANOPY',
-      year: 2020,
-      id: 4,
-      image: '/assets/img/snapshoot-protfolio-4.png',
-      technologies: ['html', 'css', 'javascript'],
-      description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-        + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
-        + 'when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem '
-        + 'Ipsum is simply dummy text of the printing and typesetting industry.',
-      liveVersion: 'https://github.com/benardop/portfolio-project',
-      source: 'https://github.com/benardop/portfolio-project',
-    },
-  ];
+    liveVersion: 'https://github.com/benardop/portfolio-project',
+    source: 'https://github.com/benardop/portfolio-project',
+  },
 
-  //= ==============================DETAILS POPUP==================================//
-//= =========GLOBAL VARIABLES====================//
+  {
+    name: 'Tonic',
+    client: 'CANOPY',
+    year: 2019,
+    id: 3,
+    image: '/image/snapshoot-portfolio3.png',
+    technologies: ['html', 'css', 'javascript'],
+    description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+        + 'when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem '
+        + 'Ipsum is simply dummy text of the printing and typesetting industry.',
+    liveVersion: 'https://github.com/benardop/portfolio-project',
+    source: 'https://github.com/benardop/portfolio-project',
+  },
+
+  {
+    name: 'Multi post Stories',
+    client: 'CANOPY',
+    year: 2020,
+    id: 4,
+    image: '/image/snapshoot-protfolio-4.png',
+    technologies: ['html', 'css', 'javascript'],
+    description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+        + "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+        + 'when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem '
+        + 'Ipsum is simply dummy text of the printing and typesetting industry.',
+    liveVersion: 'https://github.com/benardop/portfolio-project',
+    source: 'https://github.com/benardop/portfolio-project',
+  },
+];
+
 // Get the modal_content div
 const popupProjectCard = document.getElementById('popupProjectCard');
 function createProjectCard(pid) {
-  // Project title
+  // modal title
   const projName = document.createElement('div');
   projName.classList.add('popup-project-title');
   const h2 = document.createElement('h2');
   h2.innerText = projectsData[pid].name;
   projName.appendChild(h2);
   popupProjectCard.appendChild(projName);
-  // Client-frame
+  // frame
   const clientFrame = document.createElement('div');
   clientFrame.classList.add('popup-client-frame');
-  /* CLIENT NAME */
+
   const ul1 = document.createElement('ul');
   const li1 = document.createElement('li');
   const clientName = document.createElement('div');
@@ -107,7 +104,8 @@ function createProjectCard(pid) {
   clientName.appendChild(h31);
   li1.appendChild(clientName);
   ul1.appendChild(li1);
-  /* ROLE TXT */
+
+  // insert roles
   const li2 = document.createElement('li');
   const roleTxt = document.createElement('div');
   const span1 = document.createElement('span');
@@ -119,7 +117,7 @@ function createProjectCard(pid) {
   roleTxt.appendChild(h32);
   li2.appendChild(roleTxt);
   ul1.appendChild(li2);
-  /* YEAR TXT */
+
   const li3 = document.createElement('li');
   const yearTxt = document.createElement('div');
   const span2 = document.createElement('span');
@@ -131,30 +129,30 @@ function createProjectCard(pid) {
   yearTxt.appendChild(h33);
   li3.appendChild(yearTxt);
   ul1.appendChild(li3);
-  /* Finally append the whole 'ul' list to clientFrame */
+
   clientFrame.appendChild(ul1);
   popupProjectCard.appendChild(clientFrame);
-  /* ADD IMAGE SNAPSHOOT-PORTFOLIO */
+
   const imgPortfolio = document.createElement('div');
   imgPortfolio.classList.add('popup-snapshoot-portfolio');
   const img = document.createElement('img');
   img.setAttribute('src', projectsData[pid].image, 'alt', `project${pid + 1}image`);
   imgPortfolio.appendChild(img);
   popupProjectCard.appendChild(imgPortfolio);
-  /* ADD DESCRIPTION & ACTION-BUTTONS & TAGS CONTAINER */
+
   const descContainer = document.createElement('div');
   descContainer.classList.add('popup-description-container');
-  /* ADD DESCRIPTION */
+
   const description = document.createElement('div');
   description.classList.add('popup-detail-txt');
   const p = document.createElement('p');
   p.innerText = projectsData[pid].description;
   description.appendChild(p);
   descContainer.appendChild(description);
-  /* ADD TAGS & ACTION-BUTTONS CONTAINER */
+
   const actionContainer = document.createElement('div');
   actionContainer.classList.add('popup-action-container');
-  /* ADD TAGS */
+
   const tags = document.createElement('div');
   tags.classList.add('popup-tags');
   const ul2 = document.createElement('ul');
@@ -169,7 +167,7 @@ function createProjectCard(pid) {
   ul2.appendChild(li6);
   tags.appendChild(ul2);
   actionContainer.appendChild(tags);
-  /* ADD ACTION-BUTTONS */
+
   const popupAction = document.createElement('div');
   popupAction.classList.add('popup-action');
   const aLive = document.createElement('a');
@@ -189,37 +187,29 @@ function createProjectCard(pid) {
   popupProjectCard.appendChild(descContainer);
 }
 
-// Get the modal
 const modal = document.getElementById('projectDetails');
-// When the user clicks on the button, open the modal
-function popupDetails(projId) {
+
+function modalDetails(projId) {
   createProjectCard(projId - 1);
   modal.style.display = 'inline-block';
 }
 
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName('close')[0];
-// When the user clicks on <span> (x), close the modal
+// eslint-disable-next-line func-names
 span.onclick = function () {
   modal.style.display = 'none';
-  // This while removes all the CARD child-elements.
   while (popupProjectCard.firstChild) {
     popupProjectCard.removeChild(popupProjectCard.firstChild);
   }
 };
-
-// When the user clicks anywhere outside of the modal, close it
+// eslint-disable-next-line func-names
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = 'none';
-    // This while removes all the CARD child-elements.
     while (popupProjectCard.firstChild) {
       popupProjectCard.removeChild(popupProjectCard.firstChild);
     }
   }
 };
-
-/* JUST TO SKIP LINTER ERRORS */
-popupDetails.call();
-toggleMenu.call();
-/* END OF JUST TO SKIP LINTER ERRORS */
+modalDetails.call();
+modalDetails.call();
